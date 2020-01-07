f6c7e5d7ff348ae6d3c2a867839bb5e87a054ec2
Sivanandhan <835228@cognizant.com>
2020-01-07 14:18:27 +0530

    Java

---
+++ src/com/cognizant/movie/dao/FavoriteDaoCollectionImplTest.java
@@ -0,0 +1,40 @@
+package com.cognizant.movie.dao;
+
+import java.util.List;
+
+import com.cognizant.movie.model.Movie;
+
+public class FavoriteDaoCollectionImplTest {
+    static FavoriteDao favoriteDao = new FavoriteDaoCollectionImpl();
+
+    public static void main(String[] args) {
+        testAddFavorites();
+        testGetAllFavorites();
+        testRemoveFavorites();
+        testGetAllFavorites();
+    }
+
+    public static void testAddFavorites() {
+        System.out.println("Movie added into favorites");
+        favoriteDao.addFavoritesById(1, 2);// 1 is userId,2 is movieId
+        favoriteDao.addFavoritesById(1, 3);// 1 is userId,3 is movieId
+    }
+
+    public static void testRemoveFavorites() {
+        System.out.println("Movie removed from favorites");
+        favoriteDao.removeFavoritesById(1, 2);
+    }
+
+    public static void testGetAllFavorites() {
+        try {
+            System.out.println("Retrieving all  favorites");
+            List<Movie> fav = favoriteDao.getAllFavorites(1).getFavoriteList();
+            for (Movie movie : fav) {
+                System.out.println(movie);
+            }
+            System.out.println("No of Favorites :" + favoriteDao.getAllFavorites(1).getTotal());
+        } catch (FavoriteEmptyException e) {
+            System.out.println(e);
+        }
+    }
+}
